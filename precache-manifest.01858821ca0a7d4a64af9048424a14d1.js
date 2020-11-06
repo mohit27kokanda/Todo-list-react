217ae06ee5f5eb3efc8f5318dc5e628e654f78c7
Mohit kumar <mohitpalk2728@gmail.com>
2020-11-06 23:13:14 +0530

    Updates

---
+++ precache-manifest.01858821ca0a7d4a64af9048424a14d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3021948348c154656b467b39d58876",
+    "url": "/Todo-list-react/index.html"
+  },
+  {
+    "revision": "be4affe868607e21dc00",
+    "url": "/Todo-list-react/static/css/main.4f8f5761.chunk.css"
+  },
+  {
+    "revision": "6c1454b5034b5582847d",
+    "url": "/Todo-list-react/static/js/2.104b3b45.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-list-react/static/js/2.104b3b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4affe868607e21dc00",
+    "url": "/Todo-list-react/static/js/main.db554b14.chunk.js"
+  },
+  {
+    "revision": "55c6f2a41eb6b26d0226",
+    "url": "/Todo-list-react/static/js/runtime-main.bc3b547b.js"
+  }
+]);
